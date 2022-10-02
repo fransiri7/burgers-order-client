94f1b6b4853d23e2c1150bb7e6c015287426b2dd
fransiri7 <81312079+fransiri7@users.noreply.github.com>
2022-10-02 18:27:17 -0300

    Creacion del componente ModalProductDetail

--- src/modules/product/ProductsList.jsx
+++ src/modules/product/ProductsList.jsx
@@ -19,7 +19,6 @@ import {
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useAllProducts } from "./utils/apiHooks";
-import { ModalProductDetail } from "./ModalProductDetail";
 
 export function ProductsList() {
     const [products, getProductsCompleted] = useAllProducts();
