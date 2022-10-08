a6b2fe1ea3ea3009a0950b7ab5e22b96e4a32e15
fedesiri <federicosiri9@gmail.com>
2022-10-08 19:14:03 -0300

    card de ordenes en proceso

--- src/modules/order/OrderCard.jsx
+++ src/modules/order/OrderCard.jsx
@@ -1,11 +1,80 @@
+/* eslint-disable */
 import React from "react";
-import { Card } from "@mui/material";
-import CardContent from "@mui/material/CardContent";
+import { Card, Grid, IconButton } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import UnpublishedIcon from "@mui/icons-material/Unpublished";
+import DeleteIcon from "@mui/icons-material/Delete";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
 
-export function OrderCard() {
+export function OrderCard({ order }) {
     return (
-        <Card style={{ backgroundColor: "pink", border: "1px solid black", width: "100%", height: "220px", borderRadius: "10px" }}>
-            <CardContent>soy una tarjeta</CardContent>
+        <Card style={{ border: "1px solid black", width: "100%", borderRadius: "10px" }}>
+            <Grid container direction="column" style={{ backgroundColor: "red" }}>
+                <Grid
+                    item
+                    container
+                    justifyContent="space-between"
+                    alignContent="center"
+                    alignItems="center"
+                    style={{ backgroundColor: "yellow", height: "15%" }}
+                >
+                    <Grid item md={4} style={{ backgroundColor: "brown", marginLeft: "1%" }}>
+                        {order.time}
+                    </Grid>
+
+                    <Grid item container md={5} justifyContent="flex-end" style={{ backgroundColor: "violet" }}>
+                        <Grid item>
+                            <IconButton>
+                                <EditIcon />
+                            </IconButton>
+                        </Grid>
+                        <Grid item>
+                            <IconButton>
+                                <UnpublishedIcon />
+                            </IconButton>
+                        </Grid>
+                        <Grid item>
+                            <IconButton>
+                                <DeleteIcon />
+                            </IconButton>
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item style={{ backgroundColor: "green", textAlign: "center" }}>
+                    {order.name}
+                </Grid>
+                <Grid
+                    item
+                    container
+                    justifyContent="space-evenly"
+                    alignItems="center"
+                    style={{ backgroundColor: "turquoise", height: "20%" }}
+                >
+                    <Grid item md={8}>
+                        {order.address}
+                    </Grid>
+
+                    <Grid item md={4}>
+                        {order.deliveredBy}
+                    </Grid>
+                </Grid>
+
+                <Grid item container style={{ backgroundColor: "beige", height: "45%" }}>
+                    {order.products[0].name}
+                </Grid>
+
+                <Grid item container justifyContent="center" alignItems="center" style={{ backgroundColor: "orange", height: "10%" }}>
+                    <Grid item>${order.totalPrice}</Grid>
+                    <Grid item>
+                        <IconButton style={{ cursor: "default" }}>
+                            <CreditCardIcon />
+                        </IconButton>
+                    </Grid>
+                </Grid>
+                <Grid item container justifyContent="center" style={{ backgroundColor: "cornflowerblue", height: "10%" }}>
+                    <Grid item>{order.notes}</Grid>
+                </Grid>
+            </Grid>
         </Card>
     );
 }
--- src/modules/order/OrdersList.jsx
+++ src/modules/order/OrdersList.jsx
@@ -3,7 +3,101 @@ import { OrderCard } from "./OrderCard";
 import { Grid } from "@mui/material";
 
 export function OrdersList() {
-    const orders = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const orders = [
+        {
+            id: 1,
+            name: "Federico",
+            address: "San Luis 150",
+            status: false,
+            notes: "esquina colombo, sobre san luis, puerta blanca",
+            paymentMethod: "cash",
+            deliveredBy: "Cadeteria",
+            takeAway: false,
+            totalPrice: 2400,
+            time: "2022-10-05T22:10:52.563Z",
+            products: [
+                {
+                    name: "American Classic Simple",
+                    price: 650,
+                    hexColor: "#83da45",
+                    orderProduct: {
+                        id: 1,
+                        orderId: 1,
+                        productId: 1,
+                        quantity: 2,
+                        subtotal: 1300,
+                        notes: null
+                    }
+                },
+                {
+                    name: "American Classic Doble",
+                    price: 1100,
+                    hexColor: "#610e4d",
+                    orderProduct: {
+                        id: 2,
+                        orderId: 1,
+                        productId: 2,
+                        quantity: 1,
+                        subtotal: 1100,
+                        notes: null
+                    }
+                }
+            ]
+        },
+        {
+            id: 2,
+            name: "Francisco",
+            address: "",
+            status: false,
+            notes: "lo paso a retirar onda 22 hs",
+            paymentMethod: "transfer",
+            deliveredBy: "",
+            takeAway: true,
+            totalPrice: 1800,
+            time: "2022-10-05T22:15:17.535Z",
+            products: [
+                {
+                    name: "Caprese Simple",
+                    price: 600,
+                    hexColor: "#2ca5d0",
+                    orderProduct: {
+                        id: 3,
+                        orderId: 2,
+                        productId: 7,
+                        quantity: 1,
+                        subtotal: 600,
+                        notes: null
+                    }
+                },
+                {
+                    name: "Classic Burger Simple",
+                    price: 600,
+                    hexColor: "#477871",
+                    orderProduct: {
+                        id: 4,
+                        orderId: 2,
+                        productId: 13,
+                        quantity: 1,
+                        subtotal: 600,
+                        notes: null
+                    }
+                },
+                {
+                    name: "Cheesebacon Simple",
+                    price: 600,
+                    hexColor: "#901e1e",
+                    orderProduct: {
+                        id: 5,
+                        orderId: 2,
+                        productId: 9,
+                        quantity: 1,
+                        subtotal: 600,
+                        notes: "sin cebolla caramelizada por favor!"
+                    }
+                }
+            ]
+        }
+    ];
 
     return (
         <Grid container direction="column" justifyContent="space-between" style={{ backgroundColor: "blue", height: "80vh" }}>
@@ -13,9 +107,16 @@ export function OrdersList() {
             <Grid item container style={{ backgroundColor: "pink", height: "85%", overflowY: "scroll" }}>
                 {orders.map(order => {
                     return (
-                        <Grid key={order} item container justifyContent="center" md={4} style={{ marginTop: "15px", marginBottom: "5px" }}>
+                        <Grid
+                            key={order.id}
+                            item
+                            container
+                            justifyContent="center"
+                            md={4}
+                            style={{ marginTop: "15px", marginBottom: "5px" }}
+                        >
                             <Grid item md={11}>
-                                <OrderCard />
+                                <OrderCard order={order} />
                             </Grid>
                         </Grid>
                     );
