6b0047541ec5b4d4d35e008721209f16998bbaa0
fedesiri <federicosiri9@gmail.com>
2022-10-09 18:30:55 -0300

    instalacion moment.js - items establecidos dentro de la card

--- src/modules/order/OrderCard.jsx
+++ src/modules/order/OrderCard.jsx
@@ -6,11 +6,12 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import CreditCardIcon from "@mui/icons-material/CreditCard";
 import LunchDiningIcon from "@mui/icons-material/LunchDining";
 import { PropTypes } from "prop-types";
+import moment from "moment";
 
 export function OrderCard({ order }) {
     return (
         <Card style={{ border: "1px solid black", width: "100%", borderRadius: "10px" }}>
-            <Grid container direction="column" style={{ backgroundColor: "red" }}>
+            <Grid container direction="column" style={{ backgroundColor: "red", width: "100%" }}>
                 <Grid
                     item
                     container
@@ -20,7 +21,7 @@ export function OrderCard({ order }) {
                     style={{ backgroundColor: "yellow" }}
                 >
                     <Grid item md={4} style={{ backgroundColor: "brown", marginLeft: "1%" }}>
-                        {order.time}
+                        {moment(order.time).format("HH:mm")}
                     </Grid>
 
                     <Grid item container md={5} justifyContent="flex-end" style={{ backgroundColor: "violet" }}>
@@ -57,7 +58,7 @@ export function OrderCard({ order }) {
                 <Grid item container direction="column" style={{ backgroundColor: "beige" }}>
                     {order.products.map(product => (
                         <Grid
-                            key={product.id}
+                            key={product.orderProduct.id}
                             item
                             container
                             justifyContent="space-around"
@@ -79,7 +80,7 @@ export function OrderCard({ order }) {
                             <Grid item md={2} style={{ backgroundColor: "yellow" }}>
                                 ${product.price}
                             </Grid>
-                            <Grid item md={12} style={{ backgroundColor: "grey", paddingLeft: "2%" }}>
+                            <Grid item md={12} style={{ backgroundColor: "grey", paddingLeft: "10%", paddingRight: "10%" }}>
                                 {product.orderProduct.notes}
                             </Grid>
                         </Grid>
@@ -103,5 +104,31 @@ export function OrderCard({ order }) {
 }
 
 OrderCard.propTypes = {
-    order: PropTypes.object
+    order: PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        address: PropTypes.string,
+        status: PropTypes.bool,
+        notes: PropTypes.string,
+        paymentMethod: PropTypes.oneOf(["cash", "transfer"]),
+        deliveredBy: PropTypes.string,
+        takeAway: PropTypes.bool,
+        totalPrice: PropTypes.number,
+        time: PropTypes.string,
+        products: PropTypes.arrayOf(
+            PropTypes.shape({
+                name: PropTypes.string,
+                price: PropTypes.number,
+                hexColor: PropTypes.string,
+                orderProduct: PropTypes.shape({
+                    id: PropTypes.number,
+                    orderId: PropTypes.number,
+                    productId: PropTypes.number,
+                    quantity: PropTypes.number,
+                    subtotal: PropTypes.number,
+                    notes: PropTypes.string
+                })
+            })
+        )
+    })
 };
