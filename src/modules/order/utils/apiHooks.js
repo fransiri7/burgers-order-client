d0ab5e60a799a8c5bdf6e7cdce7abf65577aa5e5
fedesiri <federicosiri9@gmail.com>
2022-10-17 19:30:06 -0300

    optimizacion de codigo

--- src/modules/order/utils/apiHooks.js
+++ src/modules/order/utils/apiHooks.js
@@ -2,21 +2,13 @@ import { useState, useEffect, useCallback } from "react";
 import moment from "moment/moment";
 import * as service from "./service";
 
-const dates = (dateFrom, dateTo) => {
-    const timeDateFrom = moment(dateFrom).utcOffset(0);
-    const timeDateTo = moment(dateTo).utcOffset(0);
-    if (dateFrom && dateTo) {
-        return {
-            from: timeDateFrom.set({ hour: "00", minute: "00", second: "00" }).toISOString(),
-            to: timeDateTo.set({ hour: "23", minute: "59", second: "59" }).toISOString()
-        };
-    }
-    if (dateFrom && !dateTo) {
-        return {
-            from: timeDateFrom.set({ hour: "00", minute: "00", second: "00" }).toISOString(),
-            to: timeDateFrom.set({ hour: "23", minute: "59", second: "59" }).toISOString()
-        };
-    }
+const formatDates = (dateFrom, dateTo) => {
+    const formattedDateFrom = moment(dateFrom).utcOffset(0);
+    const formattedDateTo = moment(dateTo || dateFrom).utcOffset(0);
+    return {
+        from: formattedDateFrom.set({ hour: "00", minute: "00", second: "00" }).toISOString(),
+        to: formattedDateTo.set({ hour: "23", minute: "59", second: "59" }).toISOString()
+    };
 };
 
 export const useAllOrders = (dateFrom, dateTo) => {
@@ -24,10 +16,10 @@ export const useAllOrders = (dateFrom, dateTo) => {
     const [completed, setCompleted] = useState(false);
     const getAllOrders = useCallback(async () => {
         try {
-            const formattedDates = dates(dateFrom, dateTo);
+            const { from, to } = formatDates(dateFrom, dateTo);
 
-            const ordersByDate = await service.getOrders(formattedDates.from, formattedDates.to);
-            setOrders(ordersByDate);
+            const ordersByPeriod = await service.getOrders(from, to);
+            setOrders(ordersByPeriod);
         } catch (error) {
             console.log(error);
         } finally {
@@ -41,14 +33,3 @@ export const useAllOrders = (dateFrom, dateTo) => {
 
     return [orders, completed];
 };
-
-// Si dateFrom y dateTo existen
-// formatear dateFrom para que tenga horario a las 00 y dateTo para que tenga horario a las 23.59
-// Funcion que devuelve
-// {from: 04/05/2022 00.00, to: 10/05/2022 23.59}
-// Si dateFrom existe y dateTo es null
-// formatear dateFrom para que sea a las 00
-// formatear un segundo dateFrom para que sea a las 23.59
-// {from: 13/05/2022 00.00, to: 13/05/2022 23.59}
-
-// Haria una funcion que reciba dateFrom y dateTo y devuelva un objeto date {from: , to: }
