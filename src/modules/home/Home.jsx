37d95bf60d3d500ef11f81c04ab7b08919a90a21
fedesiri <federicosiri9@gmail.com>
2022-10-19 18:06:32 -0300

    instalacion react chart y maquetacion de grafico

--- src/modules/home/Home.jsx
+++ src/modules/home/Home.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import { React, useMemo } from "react";
+import { Grid } from "@mui/material";
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, BarElement, Title, Tooltip, Legend, Filler } from "chart.js";
+import { Bar } from "react-chartjs-2";
+ChartJS.register(CategoryScale, LinearScale, PointElement, BarElement, Title, Tooltip, Legend, Filler);
+
+const scores = [146, 204, 85, 92];
+const labels = ["week 1", "week 2", "week 3", "week 4"];
+
+const options = {
+    responsive: true,
+    scales: {
+        y: {
+            min: 0
+        }
+    },
+    plugins: {
+        legend: {
+            display: true
+        }
+    }
+};
 
 export function Home() {
+    const data = useMemo(function () {
+        return {
+            datasets: [{ label: "Last sales", data: scores, tension: 0.3, borderColor: "green", backgroundColor: "rgba(75,192,192,0.3)" }],
+            labels
+        };
+    }, []);
+
     return (
-        <img
-            src="https://www.abc.com.py/resizer/6_cADQAlpO4g57sL4wLqlIF5d7Q=/910x590/smart/arc-anglerfish-arc2-prod-abccolor.s3.amazonaws.com/public/DKHC7JKNJNGG3GBLLH6V2UDQLE.jpg"
-            alt=""
-            width="85%"
-            height="100%"
-        />
+        <Grid item container justifyContent="center">
+            <Grid item style={{ height: "80vh" }}>
+                <Bar data={data} options={options} style={{ height: "100%" }} />
+            </Grid>
+        </Grid>
     );
 }
