04db0c06a5e7986ee7d138da398002ed2cb7392d
fedesiri <federicosiri9@gmail.com>
2022-10-20 15:10:21 -0300

    colocandcionatepickers con meses y aÃños

--- src/modules/home/Home.jsx
+++ src/modules/home/Home.jsx
@@ -1,12 +1,28 @@
-/* eslint-disable */
-import { React, useMemo } from "react";
-import { Grid } from "@mui/material";
+import React from "react";
+import { Grid, TextField } from "@mui/material";
+import { blue } from "@mui/material/colors";
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, BarElement, Title, Tooltip, Legend, Filler } from "chart.js";
 import { Bar } from "react-chartjs-2";
 ChartJS.register(CategoryScale, LinearScale, PointElement, BarElement, Title, Tooltip, Legend, Filler);
 
-const scores = [146, 204, 85, 92];
-const labels = ["week 1", "week 2", "week 3", "week 4"];
+const response = [
+    {
+        label: "Week 1",
+        total: 140
+    },
+    {
+        label: "Week 2",
+        total: 190
+    },
+    {
+        label: "Week 3",
+        total: 179
+    },
+    {
+        label: "Week 4",
+        total: 112
+    }
+];
 
 const options = {
     responsive: true,
@@ -23,18 +39,29 @@ const options = {
 };
 
 export function Home() {
+    const labels = response.map(item => item.label);
+    const scores = response.map(item => item.total);
+
     const data = {
-        datasets: [{ label: "Last sales", data: scores, tension: 0.3, borderColor: "green", backgroundColor: "rgba(75,192,192,0.3)" }],
+        datasets: [
+            {
+                label: "Last sales",
+                data: scores,
+                backgroundColor: `${blue[200]}`
+            }
+        ],
         labels
     };
 
     return (
-        <Grid container direction="column" justifyContent="space-between" style={{ width: "65%", backgroundColor: "red" }}>
-            <Grid item container alignItems="center" style={{ backgroundColor: "green", height: "10%" }}>
-                <Grid item>aca va a ir el date picker</Grid>
+        <Grid container direction="column" justifyContent="space-between" style={{ width: "65%" }}>
+            <Grid item container alignItems="center" style={{ height: "10%" }}>
+                <Grid item>
+                    <TextField name="salesForMonths" label="Sales for months" type="month" value="2022-10" />
+                </Grid>
             </Grid>
-            <Grid item container justifyContent="center" style={{ backgroundColor: "yellow", height: "85%" }}>
-                <Grid item md={12} style={{ backgroundColor: "pink", backgroundPosition: "center center", backgroundSize: "cover" }}>
+            <Grid item container justifyContent="center" style={{ height: "85%" }}>
+                <Grid item md={12}>
                     <Bar data={data} options={options} />
                 </Grid>
             </Grid>
