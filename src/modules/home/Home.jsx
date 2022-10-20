b07ec476995ab41af874a104b05057a67d3f31f5
fedesiri <federicosiri9@gmail.com>
2022-10-20 11:07:42 -0300

    reestructuracion de los grids para la grafica del home

--- src/modules/home/Home.jsx
+++ src/modules/home/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { React, useMemo } from "react";
 import { Grid } from "@mui/material";
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, BarElement, Title, Tooltip, Legend, Filler } from "chart.js";
@@ -22,17 +23,20 @@ const options = {
 };
 
 export function Home() {
-    const data = useMemo(function () {
-        return {
-            datasets: [{ label: "Last sales", data: scores, tension: 0.3, borderColor: "green", backgroundColor: "rgba(75,192,192,0.3)" }],
-            labels
-        };
-    }, []);
+    const data = {
+        datasets: [{ label: "Last sales", data: scores, tension: 0.3, borderColor: "green", backgroundColor: "rgba(75,192,192,0.3)" }],
+        labels
+    };
 
     return (
-        <Grid item container justifyContent="center">
-            <Grid item style={{ height: "80vh" }}>
-                <Bar data={data} options={options} style={{ height: "100%" }} />
+        <Grid container direction="column" justifyContent="space-between" style={{ width: "65%", backgroundColor: "red" }}>
+            <Grid item container alignItems="center" style={{ backgroundColor: "green", height: "10%" }}>
+                <Grid item>aca va a ir el date picker</Grid>
+            </Grid>
+            <Grid item container justifyContent="center" style={{ backgroundColor: "yellow", height: "85%" }}>
+                <Grid item md={12} style={{ backgroundColor: "pink", backgroundPosition: "center center", backgroundSize: "cover" }}>
+                    <Bar data={data} options={options} />
+                </Grid>
             </Grid>
         </Grid>
     );
