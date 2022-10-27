7113b0d5b1f2b5f6ed111032da4647db3dbd953e
fransiri7 <81312079+fransiri7@users.noreply.github.com>
2022-10-27 00:12:16 -0300

    Se mapean los products de formData para crear selector de hambuergesas

--- src/modules/order/CreateOrEditOrder.jsx
+++ src/modules/order/CreateOrEditOrder.jsx
@@ -2,10 +2,10 @@
 import { React, useState } from "react";
 import { Button, Grid, MenuItem, Select, Switch, TextField, Typography } from "@mui/material";
 import { useAllProducts } from "../product/utils/apiHooks";
+import { useEffect } from "react";
 
 export function CreateOrEditOrder() {
-    const [products] = useAllProducts();
-    console.log(products);
+    const [products, _, completed] = useAllProducts();
     const [formData, setFormData] = useState({
         name: "",
         address: "",
@@ -14,23 +14,24 @@ export function CreateOrEditOrder() {
         deliveredBy: false,
         takeAway: false,
         totalPrice: "",
-        time: "",
         products: []
     });
-    const [errors, setErrors] = useState({});
 
-    const validate = form => {
-        const errors = {};
-        if (!form.name) {
-            errors.name = "Name is required";
+    useEffect(() => {
+        if (completed) {
+            setFormData({
+                ...formData,
+                products: [
+                    {
+                        productId: products[0].id,
+                        quantity: 1,
+                        subtotal: products[0].price,
+                        notes: ""
+                    }
+                ]
+            });
         }
-        if (!form.takeAway) {
-            if (!form.addres) {
-                errors.address = "Addres is required";
-            }
-        }
-        return errors;
-    };
+    }, [products]);
 
     const handleChange = event => {
         event.preventDefault();
@@ -38,12 +39,7 @@ export function CreateOrEditOrder() {
             ...formData,
             [event.target.name]: event.target.value
         });
-        setErrors(
-            validate({
-                ...formData,
-                [event.target.name]: event.target.value
-            })
-        );
+        console.log("probando value", formData);
     };
 
     return (
@@ -103,44 +99,50 @@ export function CreateOrEditOrder() {
                 </Grid>
             </Grid>
 
-            <Grid item container direction="column" justifyContent="space-around" alignItems="center" spacing={2}>
-                <Grid item container>
-                    <Grid item md={8}>
-                        <Select style={{ width: "96%" }}>
-                            {products.map(elem => {
-                                return (
-                                    <MenuItem key={elem.id} value={elem.id}>
-                                        {elem.name}
-                                    </MenuItem>
-                                );
-                            })}
-                        </Select>
-                    </Grid>
-                    <Grid item container md={1} alignItems="center" justifyContent="center" spacing={1}>
-                        <Grid item>
-                            <TextField label="Burgers" type="number" />
+            {formData.products.map((product, index) => {
+                console.log({ product });
+                return (
+                    <Grid key={index} item container direction="column" justifyContent="space-around" alignItems="center" spacing={2}>
+                        <Grid item container>
+                            <Grid item md={8}>
+                                <Select style={{ width: "96%" }} value={product.productId}>
+                                    {products.map(elem => {
+                                        return (
+                                            <MenuItem key={elem.id} value={elem.id}>
+                                                {elem.name}
+                                            </MenuItem>
+                                        );
+                                    })}
+                                </Select>
+                            </Grid>
+                            <Grid item container md={1} alignItems="center" justifyContent="center" spacing={1}>
+                                <Grid item>
+                                    <TextField label="Burgers" type="number" value={product.quantity} />
+                                </Grid>
+                            </Grid>
+                            <Grid item container md={1} alignItems="center" justifyContent="center" spacing={1}>
+                                <Grid item>
+                                    <TextField label="Subtotal" value={product.subtotal} />
+                                </Grid>
+                            </Grid>
+                            <Grid item container md={2} alignItems="center" justifyContent="center" spacing={1}>
+                                <Grid item>
+                                    <Button variant="outlined">+</Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="outlined">-</Button>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                        <Grid item container alignItems="center" justifyContent="center">
+                            <Grid item md={12}>
+                                <TextField label="Notes product" multiline variant="outlined" fullWidth />
+                            </Grid>
                         </Grid>
                     </Grid>
-                    <Grid item container md={1} alignItems="center" justifyContent="center" spacing={1}>
-                        <Grid item>
-                            <TextField label="Subtotal" defaultValue="$ 1500" />
-                        </Grid>
-                    </Grid>
-                    <Grid item container md={2} alignItems="center" justifyContent="center" spacing={1}>
-                        <Grid item>
-                            <Button variant="outlined">+</Button>
-                        </Grid>
-                        <Grid item>
-                            <Button variant="outlined">-</Button>
-                        </Grid>
-                    </Grid>
-                </Grid>
-                <Grid item container alignItems="center" justifyContent="center">
-                    <Grid item md={12}>
-                        <TextField label="Notes product" multiline variant="outlined" fullWidth />
-                    </Grid>
-                </Grid>
-            </Grid>
+                );
+            })}
+
             <Grid item container alignItems="center" justifyContent="center">
                 <Grid item>
                     <TextField label="Total" defaultValue="$ 5000" />
